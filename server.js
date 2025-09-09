7e9a803ef75803d69d92034a1056e45eaaaa2825
Rajnowawave <rajnowawave@gmail.com>
2025-09-09 16:58:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -398,7 +398,7 @@ app.post("/test-whatsapp", async (req, res) => {
 // Cron job (daily 10:02 AM IST) - SCHEDULED EMAILS GO TO YOUR ADDRESS
 // -------------------
 cron.schedule(
-  "55 16 * * *",
+  "00 17 * * *",
   () => {
     console.log("⏰ Running daily email + WhatsApp job...");
     sendDailyReport(); // This still goes to rajch54875@gmail.com
